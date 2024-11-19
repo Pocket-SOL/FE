282130dcb9ba4f419487b20328bd314ba57040de
inhooinu <inhooinu@gmail.com>
2024-11-19 17:24:02 +0900

    feat: Set up grouped folder (#3)

---
+++ src/pages/Start/OnboardingPage.jsx
@@ -0,0 +1,9 @@
+import Layout from "../../layouts/Layout";
+
+export default function OnboardingPage() {
+  return (
+    <Layout>
+      <h1>Onboarding Page</h1>
+    </Layout>
+  );
+}
--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/Home/HomePage";
+
+import OnboardingPage from "../pages/Start/OnboardingPage";
+import LoginPage from "../pages/Start/LoginPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage />, index: true },
+  { path: "/", element: <OnboardingPage />, index: true },
+  { path: "/login", element: <LoginPage />, index: true },
 ]);
 
 export default router;
