1dcb7ac443c405189eba76136a76dffbe0888cc6
ehdms1115@naver.com <ehdms1115@naver.com>
2024-11-20 12:58:34 +0900

    feat:SendAllowancePage-UI(#5)

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 
 import OnboardingPage from "../pages/Start/OnboardingPage";
 import LoginPage from "../pages/Start/LoginPage";
+import SendAllowancePage from "../pages/Parents/Allowance/SendAllowancePage";
 
 const router = createBrowserRouter([
   { path: "/", element: <OnboardingPage />, index: true },
   { path: "/login", element: <LoginPage />, index: true },
+  { path: "/sendallowancepage", element: <SendAllowancePage />, index: true },
 ]);
 
 export default router;
