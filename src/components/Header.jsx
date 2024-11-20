1909e5e2b5754c3889c999e8580c59c84834b198
MINO1020 <dlalsgh6772@gmail.com>
2024-11-20 10:34:12 +0900

    'header'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,26 @@
+import "./Header.css";
+
 export default function Header() {
-  return <p>header</p>;
+  return (
+    <div className="header">
+      <header className="headerObj">
+        <a href="/main" className="logo">
+          LOGO
+        </a>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/mypage">Mypage</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
---
+++ src/pages/Children/GroupPurchase/GroupPurchaseListPage.jsx
@@ -0,0 +1,9 @@
+import Layout from "../../../layouts/Layout";
+
+export default function GroupPurchase() {
+  return (
+    <Layout>
+      <div>GroupPurchase</div>
+    </Layout>
+  );
+}
