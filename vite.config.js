ca51313ac7f3519c522685905e6a07ba5a70d09a
inhooinu <inhooinu@gmail.com>
2024-11-21 19:49:58 +0900

    feat: Apply global font and enable absolute path for images (#7)

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000", // 백엔드 서버 주소 (예시: Node.js 서버)
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": "http://localhost:3000", // 백엔드 서버 주소 (예시: Node.js 서버)
+		},
+	},
+	resolve: {
+		alias: [
+			// 절대경로로 접근하기
+			{ find: "~/images", replacement: "/src/assets/images" },
+		],
+	},
 });
