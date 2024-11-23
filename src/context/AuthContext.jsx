58856fc8b2f114c57282f8fa5f2c84501529656d
inhooinu <inhooinu@gmail.com>
2024-11-23 16:21:27 +0900

    fix: Persist isAuthenticated state after login (#9)

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,29 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [authChecked, setAuthChecked] = useState(false); // 인증 여부 확인 완료 상태
+
+	useEffect(() => {
+		const checkAuthStatus = async () => {
+			try {
+				const response = await axios.get("/api/users/auth", {
+					withCredentials: true,
+				});
+				if (response.status === 200) {
+					setIsAuthenticated(true);
+				}
+			} catch (error) {
+				setIsAuthenticated(false);
+			} finally {
+				setAuthChecked(true); // 인증 확인 완료
+			}
+		};
+		checkAuthStatus();
+	}, []);
 
 	const login = () => {
 		setIsAuthenticated(true);
@@ -14,7 +34,9 @@ export function AuthProvider({ children }) {
 	};
 
 	return (
-		<AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+		<AuthContext.Provider
+			value={{ isAuthenticated, authChecked, login, logout }}
+		>
 			{children}
 		</AuthContext.Provider>
 	);
