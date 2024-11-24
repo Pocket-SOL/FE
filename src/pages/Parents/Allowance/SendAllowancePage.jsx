13f22c1b6db24bb5f55006438ce76d31389c46c8
doeuni <ehdms1115@naver.com>
2024-11-25 00:12:10 +0900

    chore:path 수정(#26)

--- src/pages/Parents/Allowance/SendAllowancePage.jsx
+++ src/pages/Parents/Allowance/SendAllowancePage.jsx
@@ -4,42 +4,44 @@ import "./SendAllowancePage.css";
 import Layout from "../../../layouts/Layout";
 
 export default function SendAllowancePage() {
-  const [amount, setAmount] = useState("");
-  const navigate = useNavigate();
-  //숫자 3자리마다 콤마추가하기
-  const localeAmount = Number(amount).toLocaleString();
-  const maxLength = 10;
+	const [amount, setAmount] = useState("");
+	const navigate = useNavigate();
+	//숫자 3자리마다 콤마추가하기
+	const localeAmount = Number(amount).toLocaleString();
+	const maxLength = 10;
 
-  const handleComplete = () => {
-    navigate("/fixedexpenselistpage", { state: { amount: localeAmount } });
-  };
-  return (
-    <div className="Container">
-      <div>하민지에게 용돈보내기</div>
-      <h1>얼마를 보낼래요?</h1>
-      <h3>{localeAmount}원</h3>
-      <p>출금 가능 잔액 원</p>
-      <div>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, "00", 0, "←"].map((num) => (
-          <button
-            key={num}
-            onClick={() => {
-              if (num === "←") {
-                setAmount(amount.slice(0, -1));
-              } else if (amount.length < maxLength) {
-                setAmount(amount + num); //10미만일때만 숫자늘어나도록
-              }
-            }}
-          >
-            {num}
-          </button>
-        ))}
-      </div>
-      <Link to="/FixedExpenseListPage" state={{ amount: localeAmount }}>
-        <button className="complete-button" onClick={handleComplete}>
-          완료
-        </button>
-      </Link>
-    </div>
-  );
+	const handleComplete = () => {
+		navigate("/parents/fixed-expense-list", {
+			state: { amount: localeAmount },
+		});
+	};
+	return (
+		<div className="Container">
+			<div>하민지에게 용돈보내기</div>
+			<h1>얼마를 보낼래요?</h1>
+			<h3>{localeAmount}원</h3>
+			<p>출금 가능 잔액 원</p>
+			<div>
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, "00", 0, "←"].map((num) => (
+					<button
+						key={num}
+						onClick={() => {
+							if (num === "←") {
+								setAmount(amount.slice(0, -1));
+							} else if (amount.length < maxLength) {
+								setAmount(amount + num); //10미만일때만 숫자늘어나도록
+							}
+						}}
+					>
+						{num}
+					</button>
+				))}
+			</div>
+			<Link to="/parents/fixed-expense-list" state={{ amount: localeAmount }}>
+				<button className="complete-button" onClick={handleComplete}>
+					완료
+				</button>
+			</Link>
+		</div>
+	);
 }
