7a0b3d8225e75dfb078b9d71cafeb671ecfc1540
0321minji <0321minji@ewhain.net>
2024-11-26 20:55:15 +0900

    Feat : make connect save photo api(#39)

--- src/libs/apis/histories.js
+++ src/libs/apis/histories.js
@@ -1,12 +1,31 @@
-const BASE_URL = "/api";
+const BASE_URL = "/api/histories";
 
 export async function fetchHistoryDetail(historyId) {
 	try {
-		const res = await fetch(`${BASE_URL}/histories?id=${historyId}`, {
+		const res = await fetch(`${BASE_URL}?id=${historyId}`, {
 			method: "GET",
 			headers: {
 				"Content-type": "application/json",
 			},
+		});
+		return await res.json();
+	} catch (error) {
+		console.error(error);
+		return {};
+	}
+}
+
+export async function uploadHistoryImage(file, directory) {
+	try {
+		// FormData 객체 생성
+		const formData = new FormData();
+		formData.append("directory", directory);
+		formData.append("image", file);
+
+		// fetch 요청
+		const res = await fetch(`${BASE_URL}/img`, {
+			method: "POST",
+			body: formData,
 		});
 		return await res.json();
 	} catch (error) {
