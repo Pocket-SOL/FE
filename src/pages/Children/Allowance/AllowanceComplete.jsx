e3051e8cb2b34f2c7f29f60e9d84790f5fea1bd0
ehdms1115@naver.com <ehdms1115@naver.com>
2024-11-27 12:34:54 +0900

    feat:Request Confirm UI(#73)

---
+++ src/pages/Children/Allowance/AllowanceComplete.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import checkImage from "~/images/check.png";
+export default function AllowanceComplete() {
+	const navigate = useNavigate();
+	return (
+		<div className="Container">
+			<img
+				src={checkImage}
+				alt="character"
+				loading="lazy"
+				style={{ width: 170 }}
+			/>
+			<p style={{ marginTop: 50 }}>조르기 완료</p>
+			<div style={{ marginTop: 10 }}>용돈 조르기 요청이 전달되었습니다.</div>
+			<div className="button-container">
+				<button
+					className="complete-button"
+					style={{ backgroundColor: "#F3F3F3", color: "black", marginTop: 100 }}
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					확인
+				</button>
+			</div>
+		</div>
+	);
+}
