d656fa8fd67d38c53de06f07e611ff9351eb0a1f
ehdms1115@naver.com <ehdms1115@naver.com>
2024-11-28 16:36:17 +0900

    feat : add answer (#109)

---
+++ src/pages/Children/Quiz/Answer.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import CorrectImg from "~/images/Correct.png";
+// import InCorrectImg from "~/images/InCorrect.png";
+
+// export default function Answer() {
+// 	return (
+// 		<div className="flex items-center justify-center ">
+// 			<img
+// 				style={{ width: 270 }}
+// 				src={CorrectImg}
+// 				className="relative top-60 left-6  z-0"
+// 			/>
+// 			{/* <img
+// 				style={{ width: 270 }}
+// 				// src={InCorrectImg}
+// 				className="absolute top-10 left-15 z-0"
+// 			/> */}
+// 			<div className="flex items-center justify-center absolute text-2xl top-48">
+// 				축하합니다 <br />
+// 				정답입니다!
+// 			</div>
+// 		</div>
+// 	);
+// }
