759b9d744238161ee4b0c965ee2cf96419bd8135
MINO1020 <dlalsgh6772@gmail.com>
2024-12-03 12:44:52 +0900

    'update-push'

--- src/pages/Parents/NotifiactionPage.jsx
+++ src/pages/Parents/NotifiactionPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const socket = io("http://localhost:5000");
 
@@ -11,6 +12,12 @@ export default function NotificationPage() {
 	const [notifications, setNotifications] = useState([]);
 	console.log(notifications);
 
+	// const handleAcceptAllowance = () => {
+
+	// };
+	// const handleRejectAllowance = () => {
+
+	// };
 	useEffect(() => {
 		//fetch하는 과정이 필요함.
 		socket.emit("register", user.user_id);
@@ -35,7 +42,19 @@ export default function NotificationPage() {
 				<ul>
 					{notifications.map((notification, index) => (
 						<li key={index}>
-							<strong>용돈요청:</strong> {notification.AllowanceMessage} <br />
+							{notification.type === "ask-allowance" && (
+								<div>
+									<strong>자녀 승인:</strong> {notification.AllowanceMessage}{" "}
+									<br />
+									<button onClick={() => handleAcceptAllowance(notification)}>
+										승인
+									</button>{" "}
+									<button onClick={() => handleRejectAllowance(notification)}>
+										거절
+									</button>
+									<br />
+								</div>
+							)}
 							{/* <strong>받은 사람 ID:</strong> {notification.receiver_id} <br /> */}
 							{/* <strong>읽음 상태:</strong>{" "} */}
 							{/* {notification.isread ? "읽음" : "읽지 않음"} */}
