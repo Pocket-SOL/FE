2fa73fd7c88e335a1d5aa14a5045c6eba8893cc6
campus3 S035 <s035@alpacoedu.onmicrosoft.com>
2024-12-05 05:05:50 +0900

    Feat : make get hisotry func(#191)

--- src/libs/apis/subaccounts.js
+++ src/libs/apis/subaccounts.js
@@ -1,10 +1,25 @@
-const BASE_URL = "/api";
+const BASE_URL = "/api/subaccounts";
 
 export async function fetchSubUsageBalance(userId) {
 	try {
-		const res = await fetch(`${BASE_URL}/subaccounts?userId=${userId}`, {
+		const res = await fetch(`${BASE_URL}?userId=${userId}`, {
 			method: "GET",
 			headers: {
+				"Content-type": "application/json",
+			},
+		});
+		return await res.json();
+	} catch (error) {
+		console.error(error);
+		return {};
+	}
+}
+
+export async function fetchSubUsageHistory(type,userId) {
+	try{
+		const res =await fetch(`${BASE_URL}/histories?type=${type}&userId=${userId}`,{
+			method:"GET",
+			headers:{
 				"Content-type": "application/json",
 			},
 		});
